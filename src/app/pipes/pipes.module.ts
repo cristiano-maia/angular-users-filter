4a31dc343343b3d7caea069997031a902d8530e1
Cristiano Maia <cristianoaj@gmail.com>
2025-05-30 16:58:18 -0300

    Pipe endereco e status

--- src/app/pipes/pipes.module.ts
+++ src/app/pipes/pipes.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PhonePipe } from './phone.pipe';
+import { AddressPipe } from './address.pipe';
+import { StatusPipe } from './status.pipe';
 
 @NgModule({
   declarations: [
-    PhonePipe
+    PhonePipe,
+    AddressPipe,
+    StatusPipe
   ],
   exports: [
-    PhonePipe
+    PhonePipe,
+    AddressPipe,
+    StatusPipe
   ],
   imports: [
     CommonModule
