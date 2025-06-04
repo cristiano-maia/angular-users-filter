a80455396492dcb8df771186b5f18653257009a5
Cristiano Maia <cristianoaj@gmail.com>
2025-06-04 10:38:34 -0300

    filtro por nome

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { UsersList } from './data/users-list';
 import { IUser } from './interfaces/user/user.interface';
+import { UsersList } from './data/users-list';
+import { IFilterOptions } from './interfaces/filter-options.interface';
 
 @Component({
   selector: 'app-root',
@@ -8,19 +9,46 @@ import { IUser } from './interfaces/user/user.interface';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-usersList: IUser[] = [];
-userSelected: IUser = {} as IUser;
-showUserDetails: boolean  = false;
+  usersList: IUser[] = [];
+  usersListFiltered: IUser[] = [];
+  userSelected: IUser = {} as IUser;
+  showUserDetails: boolean = false;
 
- ngOnInit()  {
-   setTimeout(() => {
-     this.usersList = UsersList;
-   }, 1000);
+  ngOnInit() {
+    setTimeout(() => {
+      this.usersList = UsersList;
+      this.usersListFiltered = this.usersList;
+    }, 1);
   }
 
   onUserSelected(user: IUser) {
-    console.log(user);
     this.userSelected = user;
     this.showUserDetails = true;
+  }
+
+  onFilter(filterOptions: IFilterOptions) {
+    console.log(filterOptions);
+
+    this.usersListFiltered = this.filterUsersList(filterOptions, this.usersList);
+  }
+
+  filterUsersList(filterOptions: IFilterOptions, usersList: IUser[]): IUser[] {
+    let filteredList: IUser[] = [];
+
+    filteredList = this.filterUsersListByName(filterOptions.name, usersList);
+
+    return filteredList;
+  }
+
+  filterUsersListByName(name: string | undefined, usersList: IUser[]): IUser[] {
+    const NAME_NOT_TYPPED = name === undefined;
+
+    if(NAME_NOT_TYPPED) {
+      return usersList;
+    }
+
+    const filteredList = usersList.filter((user) => user.nome.toLowerCase().includes(name.toLowerCase()));
+
+    return filteredList;
   }
 }
--- src/app/components/filter/filter.component.ts
+++ src/app/components/filter/filter.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { IFilterOptions } from 'src/app/interfaces/filter-options.interface';
 
 @Component({
@@ -17,9 +17,11 @@ export class FilterComponent {
 statusList = [
   { description: 'Ativo', value: true },
   { description: 'Inativo', value: false },
-]
+];
+
+@Output('onFilter') onFilterEmitt = new EventEmitter<IFilterOptions>();
 
 onFilter(){
-  console.log('Filter options:', this.filterOptions);
+  this.onFilterEmitt.emit(this.filterOptions);
 }
 }
